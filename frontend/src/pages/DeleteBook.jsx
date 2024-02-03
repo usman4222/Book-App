284f2bd99ed576a075fae49c79f8c8f798e15b30
usman4222 <133402116+usman4222@users.noreply.github.com>
2024-02-03 10:12:52 -0800

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -16,7 +16,7 @@ function DeleteBook() {
     const deleteBook = () => {
         setLoading(true)
         axios
-            .delete(`http://localhost:4000/books/${id}`)
+            .delete(`book-app-brown.vercel.app/${id}`)
             .then(() => {
                 setLoading(false)
                 enqueueSnackbar("Book Deleted Successfully", { variant: 'success' })
